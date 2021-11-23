d18ec4d12d341aff8058590839f68d41f8a75850
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-11-23 17:01:10 +0100

    title

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    
+    <Title />
   </div>
 </template>
 
 <script>
-
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Title
   }
 }
 </script>
